1f5a3d1eea3dba29b30594f4179c768ba66a7c17
robin3910 <robin3910@163.com>
2022-08-28 23:07:10 +0800

    update comment

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # instagram-crawler
+
+
+1、安装postman以及node
+
+2、打开instagram进行hashtag搜索
+
+3、将请求中的sections复制出来，放到postman中，生成代码，再复制到工程代码中
--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 let isRunning = false;
+
+// 预期要拉取的点赞数，比如设置了50，超过50便会将username记录下来
 let likeLimit = 50;
-let pageLimit = 20;
+// 预期要拉取的页码数，这里可以设置很大，因为每一页就10来个，且每一页不一定有目标username
+let pageLimit = 1000;
+
 const userMap = {};
+
 
 const fs = require('fs')
 const { resolve } = require('path');
@@ -11,25 +16,31 @@ console.log(filePath);
 // 返回运行文件所在的目录
 console.log('__dirname : ' + __dirname)
 
+
+/**
+ * 改中间这段代码就行
+ */
 var axios = require('axios');
 var qs = require('qs');
 var data = qs.stringify({
     'include_persistent': '0',
-    'max_id': 'QVFEUTZDejZBLVc0bTh4VlpnOXh1NS16MjEtYmxWR0o4SFBqMU5VUnJOOUhpendicU9NR3BvOVhFZ1A4dHpqbkFpN21TNXBta1dBNy1HNW00a19FZEcxYg==',
+    'max_id': 'QVFCMjNta09uZFM3dDBEWTRxTHRJLWoxcFJ2Z3FNMFQ1empWZzNMd2pPekdtemQwLW9qOW1JV1lpZUQtaHRfVC1zaGNwVGxKeHFvbW9SU3hfZF9aSG5LYg==',
+    'next_media_ids[]': '2312012820029380081',
+    'next_media_ids[]': '2108348342013788360',
     'page': '1',
     'surface': 'grid',
     'tab': 'recent'
 });
 var config = {
     method: 'post',
-    url: 'https://i.instagram.com/api/v1/tags/rapper/sections/',
+    url: 'https://i.instagram.com/api/v1/tags/rap/sections/',
     headers: {
         'authority': 'i.instagram.com',
         'accept': '*/*',
         'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8',
         'cache-control': 'no-cache',
         'content-type': 'application/x-www-form-urlencoded',
-        'cookie': 'mid=YdkfTgALAAEkbmcPqBufmiKyV3D5; ig_did=734B6C43-E743-461E-AEEF-6A635AD5BAAB; ig_nrcb=1; csrftoken=o7GqP0J0iBhkPUuAE0twu3ir6c9W4zDc; ds_user_id=55048131614; sessionid=55048131614%3AnHSnBjubeXCLji%3A19%3AAYcrDgbQQ1VXd2zD-Ngn8CAwzg8E0luLRN6jnUKx8g; dpr=1.25; datr=uDgKY0maEvUCH6O4VjE1-JLp; rur="NAO054550481316140541693230561:01f7f6fafcc137914365d22718b5a254239b0f3f03e31b7c77f6da8a128d1afe32ccc38a"; csrftoken=o7GqP0J0iBhkPUuAE0twu3ir6c9W4zDc; ds_user_id=55048131614; rur="NAO\\05455048131614\\0541693228569:01f7f52d4e0bd0dd3708f63bb91a37f31dfc2466a9122630c0237b5a1652c780ccf23de2"',
+        'cookie': 'mid=YdkfTgALAAEkbmcPqBufmiKyV3D5; ig_did=734B6C43-E743-461E-AEEF-6A635AD5BAAB; ig_nrcb=1; csrftoken=o7GqP0J0iBhkPUuAE0twu3ir6c9W4zDc; ds_user_id=55048131614; sessionid=55048131614%3AnHSnBjubeXCLji%3A19%3AAYcrDgbQQ1VXd2zD-Ngn8CAwzg8E0luLRN6jnUKx8g; dpr=1.25; datr=uDgKY0maEvUCH6O4VjE1-JLp; rur="NAO054550481316140541693234221:01f7ba6a9f09ee25fdeb7e3743dd16fc441727fdbb6cbf1033cbb99c5b68888877933ebd"; csrftoken=o7GqP0J0iBhkPUuAE0twu3ir6c9W4zDc; ds_user_id=55048131614',
         'origin': 'https://www.instagram.com',
         'pragma': 'no-cache',
         'referer': 'https://www.instagram.com',
@@ -46,8 +57,20 @@ var config = {
         'x-ig-www-claim': 'hmac.AR0hqjsW_W3X6jzPtU08tkGWKdqa5nSytDdxmdaluaAaJ93M',
         'x-instagram-ajax': '1006107265'
     },
-    data: data
+    data : data
 };
+
+axios(config)
+    .then(function (response) {
+        console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+
+/**
+ * 代码结束
+ */
 
 
 let dataObj = qs.parse(data);
